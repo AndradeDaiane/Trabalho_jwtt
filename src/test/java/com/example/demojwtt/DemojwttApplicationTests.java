b6b2253253f289e3633cd5f985dfe06a9519667e
AndradeDaiane <daiane.santosasd23@gamail.com>
2025-06-21 14:17:51 -0300

    Trabalho JWT

---
+++ src/test/java/com/example/demojwtt/DemojwttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demojwtt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemojwttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
